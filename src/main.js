43d0e2dec7bb0fe9410cce81a6add5d176cecf27
kurryLiang <13129376193@163.com>
2019-03-31 12:08:48 +0800

    新建活动

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import newActivity from './newActivity'
+
+import dateTime from 'vue-date-time-m'
+Vue.component('data-time', dateTime)
+
+Vue.config.productionTip = false
+  
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { newActivity },
+  template: '<newActivity/>'
+})
