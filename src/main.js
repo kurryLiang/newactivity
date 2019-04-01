4e3000f249ac3753000c27333c7170a0fd3854d3
kurryLiang <13129376193@163.com>
2019-04-01 22:12:03 +0800

    新建活动

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import newActivity from './newActivity'
-
+import axios from 'axios'
 import dateTime from 'vue-date-time-m'
 Vue.component('data-time', dateTime)
+
+Vue.prototype.$ajax = axios
 
 Vue.config.productionTip = false
   
--- src/newActivity.vue
+++ src/newActivity.vue
@@ -1,72 +1,80 @@
 <template>
   <div id="app">
-    <img class="close" @click="close" src="../static/img/close.png">
-			<div class="main">
-				<h6>新建活动</h6>
-				<h3>预约</h3>
-				<transition name="toChange1">
-					<div v-if="show1" id="toChange1">
-						<form class="inputbox">
-							<h5>姓名</h5>
-							<input v-model="name" id="name" class="name" type="text" name="name">
-							<h5>方向</h5>
-							<img class="more" @click="isShow" src="../static/img/more.png">
-							<input v-model="direction" id="direction" @click="show4=false" maxlength="9" class="direction" type="text" name="direction">							
-								<ul class='dire' v-if="show4">
-									<div class="dires">	
-										<li @click="getValue" value="策划方向">策划方向</li>
-										<li @click="getValue" value="产品方向">产品方向</li>
-										<li @click="getValue" value="公关方向">公关方向</li>
-										<li @click="getValue" value="新媒体方向">新媒体方向</li>
-										<li @click="getValue" value="视觉方向">视觉方向</li>
-										<li @click="getValue" value="视频方向">视频方向</li>
-										<li @click="getValue" value="摄影方向">摄影方向</li>
-										<li @click="getValue" value="Android方向">Android方向</li>
-										<li @click="getValue" value="Web方向">Web方向</li>
-										<li @click="getValue" value="IOS方向">IOS方向</li>
-									</div>
-								</ul>																			
-							<h5>电话</h5>
-							<input v-model="telephone" id="telephone" class="telephone" type="text" name="telephone">
-						</form>
-					</div>
-				</transition>	
-				<transition name="toChange2">
-					<div v-if="show2" id="toChange2">
-						<form class="inputbox">
-							<h5>标题</h5>
-							<input v-model="title" id="title" class="title" type="text" name="title">
-							<h5>标识</h5>
-							<input v-model="mark" id="mark" class="mark" type="text" name="mark">
-							<h5>地点</h5>
-							<input v-model="location" id="location" class="location" type="text" name="location">
-						</form>
-					</div>
-				</transition>
-				<transition name="toChange3">
-					<div v-if="show3" id="toChange3">
-						<form class="inputbox">
-							<h5>开始</h5>
-							 	<div class="date-time-input" @click="show"><input v-model="start" @click="getStart" id="start" class="start" type="text" name="start"></div>
-								<date-time ref="dateTime"  type="time" @confirm="select"></date-time>							
-							<h5>结束</h5>
-							  <div class="date-time-input" @click="show"><input v-model="end" @click="getEnd" id="end" class="end" type="text" name="end"></div>
-								<date-time ref="dateTime" type="time" @confirm="select"></date-time>								
-						</form>
-					</div>
-				</transition>
-			</div>
-			<div class="button">
-				<img @click="priorclick" v-if="btn" id="prior" class="prior" src="../static/img/prior.png">
-				<img @click="priorsclick" v-if="btns" class="priors" src="../static/img/priors.png">
-				<img @click="nextclick" id="next" class="next" src="../static/img/next.png">
-			</div>
+		<div v-if="unsubmit">
+			<img class="close" @click="close" src="../static/img/close.png">
+				<div class="main">
+					<h6>新建活动</h6>
+					<h3>预约</h3>
+					<transition name="toChange1">
+						<div v-if="show1" id="toChange1">
+							<form class="inputbox">
+								<h5>姓名</h5>
+								<input v-model="name" id="name" class="name" type="text" name="name">
+								<h5>方向</h5>
+								<img class="more" @click="isShow" src="../static/img/more.png">
+								<input v-model="direction" id="direction" @click="show4=false" maxlength="9" class="direction" type="text" name="direction">							
+									<ul class='dire' v-if="show4">
+										<div class="dires">	
+											<li @click="getValue" value="策划方向">策划方向</li>
+											<li @click="getValue" value="产品方向">产品方向</li>
+											<li @click="getValue" value="公关方向">公关方向</li>
+											<li @click="getValue" value="新媒体方向">新媒体方向</li>
+											<li @click="getValue" value="视觉方向">视觉方向</li>
+											<li @click="getValue" value="视频方向">视频方向</li>
+											<li @click="getValue" value="摄影方向">摄影方向</li>
+											<li @click="getValue" value="Android方向">Android方向</li>
+											<li @click="getValue" value="Web方向">Web方向</li>
+											<li @click="getValue" value="IOS方向">IOS方向</li>
+										</div>
+									</ul>																			
+								<h5>电话</h5>
+								<input v-model="telephone" id="telephone" class="telephone" type="text" name="telephone">
+							</form>
+						</div>
+					</transition>	
+					<transition name="toChange2">
+						<div v-if="show2" id="toChange2">
+							<form class="inputbox">
+								<h5>标题</h5>
+								<input v-model="title" id="title" class="title" type="text" name="title">
+								<h5>标识</h5>
+								<input v-model="mark" id="mark" class="mark" type="text" name="mark">
+								<h5>地点</h5>
+								<input v-model="location" id="location" class="location" type="text" name="location">
+							</form>
+						</div>
+					</transition>
+					<transition name="toChange3">
+						<div v-if="show3" id="toChange3">
+							<form class="inputbox">
+								<h5>开始</h5>
+									<div class="date-time-input" @click="show"><input v-model="start" @click="getStart" id="start" class="start" type="text" name="start"></div>
+									<date-time ref="dateTime"  type="time" @confirm="select"></date-time>							
+								<h5>结束</h5>
+									<div class="date-time-input" @click="show"><input v-model="end" @click="getEnd" id="end" class="end" type="text" name="end"></div>
+									<date-time ref="dateTime" type="time" @confirm="select"></date-time>								
+							</form>
+						</div>
+					</transition>
+				</div>
+				<div class="button">
+					<img @click="priorclick" v-if="btn" id="prior" class="prior" src="../static/img/prior.png">
+					<img @click="priorsclick" v-if="btns" class="priors" src="../static/img/priors.png">
+					<img @click="nextclick" id="next" class="next" src="../static/img/next.png">
+				</div>
+		</div>
+		
+		<img class="finish" v-if="submitted" @click="back" src="../static/img/submitted.png">
+		
   </div>
+
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
 import DateTime from 'vue-date-time-m';
+import axios from 'axios';
 
 (function (doc, win) {
     var docEl = doc.documentElement,
@@ -112,6 +120,8 @@ export default {
 				show4: false,
 				btn: true,
 				btns: false,
+				unsubmit: true,
+				submitted: false,
 				divindex: 1,
 				btnindex: 1
 		}	
@@ -144,12 +154,23 @@ export default {
 				this.btns = true
 			}
 			else if(this.divindex > 3){
-				this.show1 = true,
-				this.show2 = false,
-				this.show3 = false,
-				this.btn = true,
-				this.btns = false,
-				this.divindex = 1
+				var url="";
+				this.$ajax
+					.post(url,{
+					name:this.name,
+					direction:this.direction,
+					telephone:this.telephone,
+					title:this.title,
+					mark:this.mark,
+					location:this.location,
+					start:this.start,
+					end:this.end
+				})
+				.catch(err => {
+            console.log(err);
+				});
+				this.unsubmit = false,
+				this.submitted = true
 			}
   	},
   	priorclick(){
@@ -181,7 +202,10 @@ export default {
   	},
   	isShow(){               
       this.show4 = !this.show4;             
-  	},
+		},
+		back(){
+			
+		},
   	getValue(event){
       var e = event.currentTarget;
     	var states = e.innerHTML;
